0fb03b373915f7f7c39c61974fcd8a7afc2e06c3
shigehiro.ikeda <shigehiro.ikeda@excite-software.co.jp>
2021-05-02 19:40:40 +0900

    コード上の任意のSQLを実行した結果を表示するように変更

--- src/main/java/jp/co/excite_software/s_ikeda/study/IndexPageServlet.java
+++ src/main/java/jp/co/excite_software/s_ikeda/study/IndexPageServlet.java
@@ -6,6 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.Optional;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,11 +26,10 @@ public class IndexPageServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    private static final String database = "excite_study";
-    private static final String user = "excite";
-    private static final String password = "excite";
-
-    private static final String sql = "select version() as \"VERSION\";";
+    private static final String DB_PORT = "3306";
+    private static final String DB_NAME = "excite_study";
+    private static final String DB_USER = "excite";
+    private static final String DB_PSWD = "excite";
 
     private final String url;
 
@@ -37,7 +39,7 @@ public class IndexPageServlet extends HttpServlet {
     public IndexPageServlet() {
 
         String dbHost = System.getenv("DB_HOST");
-        url = "jdbc:mariadb://" + dbHost + ":3306/" + database + "?UTF-8&serverTimezone=JST";
+        url = "jdbc:mariadb://" + dbHost + ":" + DB_PORT + "/" + DB_NAME + "?UTF-8&serverTimezone=JST";
     }
 
     /**
@@ -46,35 +48,72 @@ public class IndexPageServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        String sql = "select "
+                + " TE.EMPLOYEE_NUMBER "
+                + ",TE.FAMILY_NAME "
+                + ",TE.GIVEN_NAME "
+                + ",TE.MAIL_ADDRESS "
+                + ",MP.NAME "
+                + ",TE.JOINING_DATE "
+                + "from "
+                + " TRN_EMPLOYEES TE "
+                + "left join TRN_EMPLOYEE_POSITIONS TP "
+                + " on TP.EMPLOYEE_ID = TE.ID "
+                + "left join MST_POSITIONS MP "
+                + " on MP.ID = TP.POSITION_ID ";
+
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out = response.getWriter();
 
+        out.append("<!doctype html>"
+                + "<html>"
+                + "<head>"
+                + "<meta charset=\"utf-8\">"
+                + "<style>"
+                + "table { border-collapse: collapse; }"
+                + "th, td { border: solid 1px; padding: 3px; }"
+                + "</style>"
+                + "</head>"
+                + "<body>");
+
+        out.append("<p>" + sql + "</p>");
+        out.append("<table>");
+
         try (
-                Connection connect = DriverManager.getConnection(url, user, password);
+                Connection connect = DriverManager.getConnection(url, DB_USER, DB_PSWD);
                 PreparedStatement ps = connect.prepareStatement(sql);) {
 
-            String dbVersion = "unknown";
-
             ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                dbVersion = rs.getString("VERSION");
+
+            ResultSetMetaData meta = rs.getMetaData();
+            ArrayList<String> columnNameList = new ArrayList<String>();
+            out.append("<tr>");
+            for (int i = 1; i <= meta.getColumnCount(); i++) {
+                String columnName = meta.getColumnName(i);
+                columnNameList.add(columnName);
+
+                out.append("<th>" + columnName + "</th>");
             }
+            out.append("</tr>");
 
-            out.append("<!doctype html>"
-                    + "<html>"
-                    + "<head>"
-                    + "<meta charset=\"utf-8\">"
-                    + "</head>"
-                    + "<body>");
+            while (rs.next()) {
+                out.append("<tr>");
 
-            out.append("<p>" + dbVersion + "<p>");
+                for (String columnName : columnNameList) {
+                    String value = Optional.ofNullable(rs.getString(columnName)).orElse("");
+                    out.append("<td>" + value + "</td>");
+                }
 
-            out.append("</body>"
-                    + "</html>");
+                out.append("</tr>");
+            }
         }
         catch (Exception e) {
             e.printStackTrace(out);
         }
-    }
 
+        out.append("</table>");
+
+        out.append("</body>"
+                + "</html>");
+    }
 }
